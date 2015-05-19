69cf203ad2a65905a6fb2764a513393824c0bc76
Sergey Nechiporenko <unreacheble777@gmail.com>
2015-05-19 03:25:34 +0200

    Чуть красоты в регулярку)

--- Validators/Email.php
+++ Validators/Email.php
@@ -12,7 +12,7 @@ namespace Validators;
 class Email extends AbstractValidator{
     public function isValid($value)
     {
-        if( preg_match('/([a-zA-Z0-9_\-\.]+@[a-zA-Z0-9\-]+\.[a-zA-Z]+)/',$value) == 0){
+        if( preg_match('/([\w_\-\.]+@[\w\-]+\.[\w]+)/',$value) == 0){
             $this->setError("Invalid Email.");
             return false;
         }
